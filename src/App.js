30b56663cf7379a945b87016cd5da60096f7e4b5
Archer <15900959052@163.com>
2020-04-06 04:55:59 +0800

    FileSearch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import './App.css';
-import { Row , Col, Button } from 'antd';
+import './App.scss';
+import { Row , Col } from 'antd';
+import FileSearch from './components/FileSearch'
 
 function App() {
   return (
     <div className="App">
-      <Button type="primary">Primary</Button>
        <Row>
-          <Col span={4}>col</Col>
-          <Col span={20}>col</Col>
+          <Col span={8} className='slideWrap'>
+            <FileSearch title='查看内容' onFileSearch={(value)=>{console.log(value)}} />
+          </Col>
+          <Col span={16} className='content'>col</Col>
         </Row>
     </div>
   );
---
+++ src/components/FileSearch.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect,useRef } from 'react'
+import { Button, Input } from 'antd';
+
+const FileSearch = (props) => {
+	const { title, onFileSearch } = props
+	const [inputActive, setInputActive] = useState(false);
+	const [value, setValue] = useState('');
+  const countRef = useRef(null);
+
+	useEffect(() => {
+		const keyUpHandler = (props) => {
+			if (props.key == 'Enter' && inputActive) {
+				setInputActive(false)
+				onFileSearch(value)
+			}
+			if (props.key == 'Escape' && inputActive) {
+				setInputActive(false)
+			}
+		}
+		document.addEventListener('keyup', keyUpHandler)
+		return () => {
+			document.removeEventListener('keyup', keyUpHandler)
+		}
+	})
+
+	useEffect(() => {
+		inputActive && countRef.current.focus();
+	},[inputActive])
+	
+	const handleSearch = ()=>{
+		setInputActive(true)
+	}
+
+	return (
+		<div>
+			{
+				!inputActive &&
+				<div className='slide'>
+					<span>{title}</span>
+					<Button type="primary" onClick={ handleSearch } >搜索</Button>
+				</div>
+			}
+			{
+				inputActive &&
+				<div className='slide'>
+					<Input placeholder="请输入" ref={countRef} onChange={(e) => { setValue(e.target.value) }} />
+					<Button type="primary" onClick={() => { setInputActive(false) }} >关闭</Button>
+				</div>
+			}
+		</div>
+	)
+
+
+}
+export default FileSearch
